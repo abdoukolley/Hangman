2957c665feb8ed1459f044e351ed1090e2d0f3de
abdoukolley <abdou.kolley24@gmail.com>
2023-10-28 01:15:09 +0100

    Checking if guess is in word

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -2,15 +2,14 @@ import random
 
 # Created a list of words to be used in the game
 word_list = ['apple', 'banana', 'orange', 'pear','pineapple']
-print(word_list)
 
 # Picks a random word from the word_list
 word = random.choice(word_list)
-print(word)
 
-
+# Guess a letter
 guess = input("Guess a letter: ")
 
+# Checking if the guess is a letter and only one letter
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
---
+++ hangman/milestone_3.py
@@ -0,0 +1,23 @@
+import random
+
+# Created a list of words to be used in the game
+word_list = ['apple', 'banana', 'orange', 'pear','pineapple']
+
+# Picks a random word from the word_list
+word = random.choice(word_list)
+
+
+while True: 
+    # Guess a letter
+    guess = input("Guess a letter: ")
+    
+    # Checking if the guess is a letter and only one letter
+    if len(guess) == 1 and guess.isalpha():
+        if guess in word:
+            print(f"Good guess!{guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word.Try again.")
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+        
+
